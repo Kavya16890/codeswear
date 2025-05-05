9475d4b2704f0714cd88039fae8535ae4c86e50a
Kavya16890 <131844765+Kavya16890@users.noreply.github.com>
2025-05-05 17:23:54 +0530

    added a navbar and changed some design

---
+++ src/components/Navbar.js
@@ -0,0 +1,36 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import { FaCartArrowDown } from "react-icons/fa";
+
+const Navbar = () => {
+  return (
+    <div className="flex flex-col justify-center md:flex-row md:justify-start items-center py-2">
+      <div className="logo mx-5">
+        <Image width={200} height={40} src="/logo.png" />
+      </div>
+      <div className="nav font-bold">
+        <ul className="flex items-center space-x-2 md:text-xl">
+          <Link href="/tshirts">
+            <li>T-shirts</li>
+          </Link>
+          <Link href="/hoodies">
+            <li>Hoodies</li>
+          </Link>
+          <Link href="/mugs">
+            <li>Mugs</li>
+          </Link>
+          <Link href="/stickers">
+            <li>Stickers</li>
+          </Link>
+        </ul>
+      </div>
+      <div className="cart absolute top-4 right-0 mx-5">
+        <FaCartArrowDown className="cursor-pointer text-xl md:text-2xl" />
+        <p>Cart</p>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
